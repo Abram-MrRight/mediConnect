dad592cfbe1ced8f80e00404d928d10a5e4b7777
Iraku Harry <iraqooh@gmail.com>
2024-10-27 10:55:02 +0300

    Setup testing and CI

---
+++ tests/Feature/BasicRoutesTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BasicRoutesTest extends TestCase
+{
+    /**
+     * Test the home route renders the dashboard view.
+     */
+    public function test_home_route_displays_dashboard()
+    {
+        $response = $this->get(route('home'));
+        $response->assertStatus(200); // Check that the status code is 200
+        $response->assertViewIs('dashboard'); // Check that it renders the correct view
+    }
+
+    /**
+     * Test the dashboard route renders the dashboard view.
+     */
+    public function test_dashboard_route_displays_dashboard()
+    {
+        $response = $this->get(route('dashboard'));
+        $response->assertStatus(200);
+        $response->assertViewIs('dashboard');
+    }
+
+    /**
+     * Test the appointments route renders the appointments view.
+     */
+    public function test_appointments_route_displays_appointments()
+    {
+        $response = $this->get(route('appointments'));
+        $response->assertStatus(200);
+        $response->assertViewIs('appointments');
+    }
+}
